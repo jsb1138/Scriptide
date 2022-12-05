1f396f0a42e342c22472bb3be6484210ee218f8b
Brent Curriden <bjcurriden@gmail.com>
2022-12-05 15:46:02 +0100

    recorded state

--- src/components/excalidrawComponent/ExcalComponent.tsx
+++ src/components/excalidrawComponent/ExcalComponent.tsx
@@ -5,17 +5,26 @@ import { Flex } from "amazon-chime-sdk-component-library-react";
 import { centerScrollOn } from "@excalidraw/excalidraw/types/scene/scroll";
 
 function ExcalComponent() {
-  const { excalActive, transitionState, opacity, setOpacity } = useScriptideContext();
+  const { excalActive, transitionState, opacity, setExcalState, excalState } =
+    useScriptideContext();
 
   return (
     <>
       <div className="hider-container">
-        <div className={`hider ${transitionState ? "open" : "closed"} ${opacity ? "opaque" : "transparent"}`}>
-        </div>
+        <div
+          className={`hider ${transitionState ? "open" : "closed"} ${
+            opacity ? "opaque" : "transparent"
+          }`}
+        ></div>
       </div>
       <div className={excalActive ? "excal-open" : "excal-closed"}>
         <div className="excalibox">
-          <Excalidraw />
+          <Excalidraw
+            onChange={(data) => {
+              setExcalState(data);
+              console.log(excalState);
+            }}
+          />
         </div>
       </div>
     </>
