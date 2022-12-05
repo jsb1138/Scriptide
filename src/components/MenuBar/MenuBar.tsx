8d869e5d62e356a86ef15d0924b5baa1533d52cd
Konstantin Richter <konstantinwfrichter@gmail.com>
2022-12-05 17:53:05 +0100

    made tooltips for ide-closed status as well

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -1,12 +1,10 @@
-
-import { useScriptideContext } from "../../contexts/ScriptideProvider";
-import { FC } from "react";
-import { ThemeDropdown } from "./ThemeDropdown";
-import { LanguageDropdown } from "./LanguageDropdown";
-import SendToNotion from "../SendToNotion/SendToNotion"
-import Tooltip from "../Tooltip/Tooltip"
-import "./MenuBar.css"
-
+import { useScriptideContext } from '../../contexts/ScriptideProvider';
+import { FC } from 'react';
+import { ThemeDropdown } from './ThemeDropdown';
+import { LanguageDropdown } from './LanguageDropdown';
+import SendToNotion from '../SendToNotion/SendToNotion';
+import Tooltip from '../Tooltip/Tooltip';
+import './MenuBar.css';
 
 const MenuBar: FC = () => {
   const {
@@ -45,58 +43,63 @@ const MenuBar: FC = () => {
     setShowLanguage(!showLanguage);
   };
 
-
   return !ideActive ? (
     <>
-      <div id="menu" className={menuState ? "menu-open" : "menu-closed"}>
-        <div className="menu-item" onClick={handleExcali}>
-          <img src="src/assets/excalidraw.png" className="excali-logo" title="open excalidraw" />
-        </div>
-        <div className='menu-item'>
-          <Tooltip content="Post to Notion">
-          <SendToNotion/>
-          </Tooltip>
-        </div>
-        </div>
-      <div
-        className={menuState ? "menu-btn-mod" : "menu-btn"}
-        onClick={toggleMenu}
-      >
-        {menuState ? "◄" : "►"}
-      </div>
-    </>
-    ) : (
-      <>
-      <div id="menu" className={menuState ? "menu-open" : "menu-closed"}>
-        <Tooltip content="Sketch diagrams with Excalidraw">
-        <div className="menu-item" onClick={handleExcali}>
-          <img src="src/assets/excalidraw.png" className="excali-logo" />
-        </div>
+      <div id='menu' className={menuState ? 'menu-open' : 'menu-closed'}>
+        <Tooltip content='Sketch diagrams with Excalidraw'>
+          <div className='menu-item' onClick={handleExcali}>
+            <img
+              src='src/assets/excalidraw.png'
+              className='excali-logo'
+              title='open excalidraw'
+            />
+          </div>
         </Tooltip>
-        <Tooltip content="Post notes with Notion">
-        <div className='menu-item'>
-          <SendToNotion/>
-        </div>
-          </Tooltip>
-          <Tooltip content="Choose an IDE theme">
-        <div className="menu-item" onClick={handleTheme}>
-          <p>T</p>
-        </div>
-          </Tooltip>
-          <Tooltip content="Choose a programming language">
-        <div className="menu-item" onClick={handleLanguage}>
-          <p>L</p>
-        </div>
-          </Tooltip>
+        <Tooltip content='Post to Notion'>
+          <div className='menu-item'>
+            <SendToNotion />
+          </div>
+        </Tooltip>
       </div>
       <div
-        className={menuState ? "menu-btn-mod" : "menu-btn"}
+        className={menuState ? 'menu-btn-mod' : 'menu-btn'}
         onClick={toggleMenu}
       >
-        {menuState ? "◄" : "►"}
+        {menuState ? '◄' : '►'}
       </div>
     </>
-    )
+  ) : (
+    <>
+      <div id='menu' className={menuState ? 'menu-open' : 'menu-closed'}>
+        <Tooltip content='Sketch diagrams with Excalidraw'>
+          <div className='menu-item' onClick={handleExcali}>
+            <img src='src/assets/excalidraw.png' className='excali-logo' />
+          </div>
+        </Tooltip>
+        <Tooltip content='Post notes with Notion'>
+          <div className='menu-item'>
+            <SendToNotion />
+          </div>
+        </Tooltip>
+        <Tooltip content='Choose an IDE theme'>
+          <div className='menu-item' onClick={handleTheme}>
+            <p>T</p>
+          </div>
+        </Tooltip>
+        <Tooltip content='Choose a programming language'>
+          <div className='menu-item' onClick={handleLanguage}>
+            <p>L</p>
+          </div>
+        </Tooltip>
+      </div>
+      <div
+        className={menuState ? 'menu-btn-mod' : 'menu-btn'}
+        onClick={toggleMenu}
+      >
+        {menuState ? '◄' : '►'}
+      </div>
+    </>
+  );
 };
 
 export default MenuBar;
