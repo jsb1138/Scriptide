f8cb57a7c1811457bec34fd7a9dcebdda7381527
Konstantin Richter <konstantinwfrichter@gmail.com>
2022-12-05 17:43:19 +0100

    added tooltips to menu bar

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -3,7 +3,8 @@ import { useScriptideContext } from "../../contexts/ScriptideProvider";
 import { FC } from "react";
 import { ThemeDropdown } from "./ThemeDropdown";
 import { LanguageDropdown } from "./LanguageDropdown";
-import SendToNotion from "../../components/SendToNotion/SendToNotion"
+import SendToNotion from "../SendToNotion/SendToNotion"
+import Tooltip from "../Tooltip/Tooltip"
 import "./MenuBar.css"
 
 
@@ -52,7 +53,9 @@ const MenuBar: FC = () => {
           <img src="src/assets/excalidraw.png" className="excali-logo" title="open excalidraw" />
         </div>
         <div className='menu-item'>
+          <Tooltip content="Post to Notion">
           <SendToNotion/>
+          </Tooltip>
         </div>
         </div>
       <div
@@ -65,18 +68,26 @@ const MenuBar: FC = () => {
     ) : (
       <>
       <div id="menu" className={menuState ? "menu-open" : "menu-closed"}>
+        <Tooltip content="Sketch diagrams with Excalidraw">
         <div className="menu-item" onClick={handleExcali}>
-          <img src="src/assets/excalidraw.png" className="excali-logo" title="open excalidraw" />
+          <img src="src/assets/excalidraw.png" className="excali-logo" />
         </div>
+        </Tooltip>
+        <Tooltip content="Post notes with Notion">
         <div className='menu-item'>
           <SendToNotion/>
         </div>
+          </Tooltip>
+          <Tooltip content="Choose an IDE theme">
         <div className="menu-item" onClick={handleTheme}>
           <p>T</p>
         </div>
+          </Tooltip>
+          <Tooltip content="Choose a programming language">
         <div className="menu-item" onClick={handleLanguage}>
           <p>L</p>
         </div>
+          </Tooltip>
       </div>
       <div
         className={menuState ? "menu-btn-mod" : "menu-btn"}
---
+++ src/components/Tooltip/Tooltip.tsx
@@ -0,0 +1,40 @@
+import "./Tooltip.css"
+import React, { useState } from "react";
+
+interface Props {
+  content: string;
+  children: React.ReactNode;
+}
+
+function Tooltip({content, children}: Props) {
+  let timeout: NodeJS.Timeout;
+  const [active, setActive] = useState(false);
+
+  const showTip = () => {
+    timeout = setTimeout(() => {
+      setActive(true);
+    }, 100);
+  };
+
+  const hideTip = () => {
+    clearInterval(timeout);
+    setActive(false);
+  };
+
+  return (
+    <div
+      className="tooltip-wrapper"
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
+    >
+      {children}
+      {active && (
+        <div className="tooltip-tip">
+          {content}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Tooltip
